258468aea8d464fda6132f71307fd66c92f55d53
Phil Burrows <github@philburrows.com>
2015-10-03 00:00:13 -0500

    still working on the resolver piece, but it's getting there

--- test/plot/resolver_test.exs
+++ test/plot/resolver_test.exs
@@ -2,6 +2,11 @@ defimpl Plot.Resolution, for: Plot.Object do
   def resolve(%Plot.Object{name: "user"} = _obj, _context) do
     %{"firstName" => "Phil", "lastName" => "Burrows", dontInclude: "this"}
   end
+
+  def resolve(%Plot.Object{name: "birthday"}, _context) do
+    IO.puts "resolving the object 'birthday'"
+    %{"day" => 15, "month" => 12, "year" => 2015}
+  end
 end
 
 defmodule Plot.ResolverTest do
@@ -11,6 +16,12 @@ defmodule Plot.ResolverTest do
   test "resolution of a simple user query" do
     doc = "{user { firstName, lastName }}" |> Plot.parse_and_generate
 
-    assert [%{"firstName" => "Phil", "lastName" => "Burrows"}] == Resolver.resolve(Enum.at(doc.operations, 0))
+    assert [%{"user" => %{"firstName" => "Phil", "lastName" => "Burrows"}}] == Resolver.resolve(Enum.at(doc.operations, 0))
+  end
+
+  test "resolution of a user with a birthday" do
+    doc = "{user {firstName, lastName, birthday {month, year}}}" |> Plot.parse_and_generate
+    assert [%{"user" => %{"firstName" => "Phil", "lastName" => "Burrows", "birthday" => %{"month" => 12, "year" => 2015}}}] ==
+            Resolver.resolve(Enum.at(doc.operations, 0))
   end
 end
