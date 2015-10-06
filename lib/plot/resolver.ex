7c09c1a573d8454c84c0161e7dc7324275440dd4
Phil Burrows <github@philburrows.com>
2015-10-06 16:59:50 -0500

    cleanup

--- lib/plot/resolver.ex
+++ lib/plot/resolver.ex
@@ -32,8 +32,7 @@ defmodule Plot.Resolver do
   defp _resolve_fields([], _context, acc), do: acc
   defp _resolve_fields([head|tail], context, acc) do
     case head do
-      %Object{} ->
-        _resolve_fields(tail, context, Dict.merge(acc, _resolve(head, Resolution.resolve(head, context))))
+      %Object{} -> _resolve_fields(tail, context, Dict.merge(acc, _resolve(head, Resolution.resolve(head, context))))
       _field    -> _resolve_fields(tail, context, Dict.put(acc, head.alias || head.name, Resolution.resolve(head, context)))
     end
   end
